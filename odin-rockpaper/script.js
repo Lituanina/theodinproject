b534acd41190df51c5400f6781d37180725c96eb
lituanina <lituanina@gmail.com>
2021-10-13 00:52:43 +0200

    first commit

--- odin-rockpaper/script.js
+++ odin-rockpaper/script.js
@@ -1,3 +1,8 @@
+let playerScore = 0;
+let computerScore = 0;
+
+
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -13,21 +18,36 @@ function computerPlay() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-      console.log("It's a tie");
+      return ("It's a tie");
     } else if (playerSelection == "rock" && computerSelection == "scissors"
       || playerSelection == "scissors" && computerSelection == "paper"
       || playerSelection == "paper" && computerSelection == "rock"){
+      playerScore++;
       return ("You win! " + playerSelection + " beats " + computerSelection);
       //
     } else {
+      computerScore++;
       return ("You lose! " + computerSelection + " beats " + playerSelection );
 
     }
   }
 
-let playerSelection = "ROCK";
-playerSelection = playerSelection.toLowerCase();
+
+function game() {
+  for (let i = 0; i < 5; i++) {
+    let playerSelection = prompt("Please choose your weapon", "rock");
+    playerSelection = playerSelection.toLowerCase();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+    console.log("Computer: " + computerScore + " Player: " + playerScore);
+  } if (computerScore === playerScore) {
+    return ("Hey! It's a tie.");
+  } else if (computerScore > playerScore) {
+    return ("What a shame! The computer won!");
+  } else {
+    return ("YOU WON!")
+  }
+}
 
 
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
